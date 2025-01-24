3ec8d9a9348f18b67bfed7eca6bdb0b294a540e9
Egor <eklusevich@gmail.com>
2025-01-24 14:36:00 +0300

    fix3

---
+++ src/pages/test.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+const test = () => {
+  return (
+    <div>
+        <Header />
+        <h1>Test page</h1>
+    </div>
+  )
+}
+
+export default test
